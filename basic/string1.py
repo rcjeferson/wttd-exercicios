0c0308effafd0c7bc03afdb1c325722d91918270
Jeferson Costa <jeferson.costa@instruct.com.br>
2019-09-14 11:00:42 -0300

    Adiciona resolução da lista string1

--- basic/string1.py
+++ basic/string1.py
@@ -24,8 +24,14 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-    # +++your code here+++
-    return
+    message = 'Number of donuts: '
+
+    if count < 10:
+        ret = message + str(count)
+    else:
+        ret = message + "many"
+
+    return ret
 
 
 # B. both_ends
@@ -34,8 +40,12 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-    # +++your code here+++
-    return
+    if len(s) < 2:
+        ret = ''
+    else:
+        ret = s[:2] + s[-2:]
+    
+    return ret
 
 
 # C. fix_start
@@ -48,8 +58,12 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    # +++your code here+++
-    return
+    first = s[0]
+    others = s[1:]
+    
+    ret = first + others.replace(first, '*')
+
+    return ret
 
 
 # D. MixUp
@@ -60,8 +74,12 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-    # +++your code here+++
-    return
+    stra = b[:2] + a[2:]
+    strb = a[:2] + b[2:]
+
+    ret = stra + ' ' + strb
+
+    return ret
 
 
 # Provided simple test() function used in main() to print
