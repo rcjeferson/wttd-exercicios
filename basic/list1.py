4215b17ccaa13dff8b11ccc1cc14b4377e505b6a
Jeferson Costa <jeferson.costa@instruct.com.br>
2019-09-14 11:25:42 -0300

    Adiciona resolução da list1

--- basic/list1.py
+++ basic/list1.py
@@ -22,8 +22,13 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-    # +++your code here+++
-    return
+    count = 0
+
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            count += 1
+    
+    return count
 
 
 # B. front_x
@@ -34,8 +39,21 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return
+    xgroup = []
+    agroup = []
+
+    for word in words:
+        if word[0] == 'x':
+            xgroup.append(word)
+        else:
+            agroup.append(word)
+
+    xgroup.sort()
+    agroup.sort()
+
+    final = xgroup + agroup
+        
+    return final
 
 
 # C. sort_last
@@ -45,9 +63,11 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
-    return
+    tuples.sort(key=takeSecond)
+    return tuples
 
+def takeSecond(tuple):
+    return tuple[1]
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
